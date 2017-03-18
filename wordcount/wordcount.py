35700a9e6d74ef46f6fcd25a6bca41ad29ab260a
arnaud <songac.arnaud@gmail.com>
2017-03-18 13:05:15 -0400

    Fix wordcount with HDFS file paths

--- wordcount/wordcount.py
+++ wordcount/wordcount.py
@@ -13,11 +13,13 @@ SPARK_CONF = SparkConf().setAppName("wordcount").setMaster("local")
 SPARK_CONTEXT = SparkContext(conf=SPARK_CONF)
 
 # IO Sources
+HDFS_LOCAL_ACCESS = "file://"
+
 WORKING_DIR = os.getcwd()
 DATA_PATH = WORKING_DIR+"/../data/"
 DATA_AZLYRICS_PATH = DATA_PATH+"/azlyrics/"
-WORD_COUNT_OUTPUT = "output/counts/"
-OUTPUT = "output/"
+WORD_COUNT_OUTPUT = WORKING_DIR + "/output/counts/"
+OUTPUT = WORKING_DIR + "/output/"
 DIVERSITY_REGULAR_FILE = OUTPUT + 'diversity_regular.txt'
 SONG_VERCTOR = OUTPUT+'song_vectors.txt'
 
@@ -31,9 +33,9 @@ f.close()
 # Count occurences of each different word an artist uses
 def wordCountByArtist(artist, filePath, version='regular'):
     
-    outputFile = ''.join([WORD_COUNT_OUTPUT, version, '_', artist])
+    outputFile = ''.join([HDFS_LOCAL_ACCESS, WORD_COUNT_OUTPUT, version, '_', artist])
     
-    counts = SPARK_CONTEXT.textFile(filePath).\
+    counts = SPARK_CONTEXT.textFile(HDFS_LOCAL_ACCESS + filePath).\
         flatMap(lambda x: x.split()).\
         map(lambda x: (x,1)).\
         reduceByKey(lambda x,y: x+y).\
@@ -56,24 +58,24 @@ def processData(path):
 # Analyze songs individually
 def songProcessing(artist, filePath, version = 'regular'):
     
-    lines = SPARK_CONTEXT.textFile(filePath).\
+    lines = SPARK_CONTEXT.textFile(HDFS_LOCAL_ACCESS + filePath).\
         flatMap(lambda x: x.split('\n'))
     print("Nb songs for " + artist+ ": ", lines.count())
-    lines.foreach(lambda x: buildSongVector(x, artist))
+    lines.foreach(lambda x: buildSongVectorSVM(x, artist))
     
 
 # Build song vector where dimensions are the following: nb of words, nb unique tokens, profane (eventually
-def buildSongVector(song, artist):
+# use libSVM format: <label> <index1>:<value1> <index2>:<value2>
+# use song length and diversity
+def buildSongVectorSVM(song, artist):
     
     song = str(song).lower()
     total_tokens = nltk.word_tokenize(song)
     uniqueToken = set(total_tokens)
-    entry = ''.join([artist, ':', str(len(total_tokens)),',' , str(len(uniqueToken)), '\n'])
+    entry = ''.join([artist, ' 1:', str(len(total_tokens)),' 2:' , str(len(uniqueToken)), '\n'])
     f = open(SONG_VERCTOR, 'a')
     f.write(entry)
     f.close()
-
-
 
 if __name__ == '__main__':
     
