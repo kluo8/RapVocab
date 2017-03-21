aec86f5f2c2a1853a1a656a5a0d941ca167d95af
arnaud <songac.arnaud@gmail.com>
2017-03-21 18:34:18 -0400

    Generate bar chart for the nb of artist songs for each clusters

--- clustering/clustersongvectors.py
+++ clustering/clustersongvectors.py
@@ -7,7 +7,7 @@ from pyspark.ml.clustering import KMeans as dfKMeans
 from pyspark.sql import SparkSession
 from pyspark.mllib.clustering import KMeansModel, KMeans
 from pyspark.mllib.linalg import Vectors
-from clustering.plotcluster import plot2DSparkCluster
+from clustering.plotcluster import plotDiversitySizeClustering, barChartArtistCountInCluster
 
 '''
 Created on Mar 18, 2017
@@ -22,10 +22,10 @@ WORKING_DIR = os.getcwd()
 SONG_VECTORS_FILE = WORKING_DIR + "/../wordcount/output/song_vectors.txt"
 ARTIST_ID_FILE = WORKING_DIR + "/../wordcount/output/artist_id.txt"
 
-CENTERS = [[1.0, 1.0],
-           [2500.0, 1.0],
-           [0.0, 700.0],
-           [2500.0, 700.0]]
+CENTERS = [[500.0, 0.0],
+           [400.0, 400.0],
+           [1500.0, 130.0],
+           [1000.0, 500.0]]
 
 CENTER_VECTORS = map(lambda center: Vectors.dense(center), CENTERS)
 
@@ -35,7 +35,6 @@ def extractArtistId():
     for line in open(ARTIST_ID_FILE):
         line = line.rstrip('\n').split()
         ARTIST_ID[int(line[1])] = line[0]
-        print(int(line[1]))
 
 '''
 Perform kmeans clustering with an prior estimation of the centroids
@@ -46,19 +45,14 @@ Perform kmeans clustering with an prior estimation of the centroids
     nb of clusters desired
 '''
 def kmeansEstimator(dataset, nClusters):
-    print("Fit kmeans model to dataset")
     kmeans = dfKMeans(k=nClusters)
     model=kmeans.fit(dataset)
-        
-    print("Show centroids")
+
     centers = model.clusterCenters()
-    print(centers)
-    
-    print("Apply model to data and show result")
+
     result = model.transform(dataset)
-    result.show()
-    plot2DSparkCluster(result.collect(), centers, "Size", "Diversity", "Song Analysis by Size and Diversity")
-    
+    plotDiversitySizeClustering(result.collect(), centers, "Size", "Diversity", "Song Analysis by Size and Diversity with Kmens||")
+
 
 ''''
 Perform Kmeans Clustering with centroids predefined
@@ -68,7 +62,7 @@ Perform Kmeans Clustering with centroids predefined
 def kmeansInitialClusters(dataset):
     model = KMeansModel(CENTER_VECTORS)
     vectorsRdd = dataset.rdd.map(lambda data: Vectors.parse(Vectors.stringify(data['features'])))
-    trainedModel = KMeans.train(vectorsRdd, 4, initialModel=model)
+    trainedModel = KMeans.train(vectorsRdd, 4, maxIterations=1000, initialModel=model)
     result=[]
     for d in dataset.collect():
         entry = {}
@@ -76,56 +70,52 @@ def kmeansInitialClusters(dataset):
         entry["prediction"] = trainedModel.predict(Vectors.parse(Vectors.stringify(d['features'])))
         entry["label"] = d['label']
         result.append(entry)
-        
-    plot2DSparkCluster(result, CENTERS, "Size", "Diversity", "Song Analysis by Size and Diversity")
+
+    plotDiversitySizeClustering(result, CENTERS, "Size", "Diversity", "Song Analysis by Size and Diversity with Initial Clusters")
     centroidArtistSongCount(result, CENTERS)
-    
+
 '''
 Count the number of songs each artist has for each cluster
 
 :param result:
-    collection with following entry format: 
+    collection with following entry format:
     {label=<artist id>, features=<song vector>, prediction=<cluster index>}
 
 '''
 def centroidArtistSongCount(result, centroids):
     artists={}
-    
+
     for entry in result:
-        print(int(entry["label"]))
         artist = ARTIST_ID[int(entry["label"])]
         centroid = str(entry["prediction"])
         if artist not in artists:
             artists[artist] = {}
             for i in range(0, len(centroids)):
                 artists[artist][str(i)] = 0
-        
+
         artists[artist][centroid]+= 1
-    
-    for artist, value in artists.items():
-        for key, value in value.items():
-            print(str(artist) + " - " + str(key) + ": " + str(value))
-    
+
+    barChartArtistCountInCluster(artists, centroids)
+
 
 if __name__ == '__main__':
-    
+
     extractArtistId()
-    
+
     print("Initialize Spark session")
     spark = SparkSession \
         .builder \
         .appName("Lab session") \
         .getOrCreate()
-    
+
     print("Read dataset")
     dataset = spark.read.format("libsvm").load(HDFS_LOCAL_ACCESS + SONG_VECTORS_FILE)
-    
-    kmeansEstimator(dataset, 4)
+
+#     kmeansEstimator(dataset, 4)
     kmeansInitialClusters(dataset)
-    
-    
+
+
     spark.stop()
-    
-    
-    
-    
+
+
+
--- clustering/plotcluster.py
+++ clustering/plotcluster.py
@@ -7,10 +7,12 @@ Created on Mar 18, 2017
 
 @author: arno
 '''
+ax = None
 
+COLORS = ['b', 'c', 'y', 'm']
 
 '''
-plot the cluster 
+plot 2D cluster
 result entry format: Row(label=1.6114804284613683e+18, features=SparseVector(2, {0: 778.0, 1: 438.0}), prediction=3)
 clusterCenters: coordinates of centroids
 '''
@@ -19,52 +21,116 @@ def plot2DSparkCluster(result, clusterCenters, xAxis="x", yAxis="y", title=''):
     x=[]
     y=[]
     area=[]
-    
+
     for r in result:
         vector = r['features']
         c = r['prediction']
         xx = vector[0]
         yy = vector[1]
-        color.append(int(c))
+        color.append(COLORS[int(c)])
         area.append(math.pi*6**2)
-        x.append(xx)
-        y.append(yy)
-    
-    plt.scatter(x,y,c=color,s=area)
-    plt.xlabel(xAxis)
-    plt.ylabel(yAxis)
-    plt.title(title)
-    
-    color=[]
-    x=[]
-    y=[]
-    area=[]
-    
-    for center in clusterCenters:
-        xx = center[0]
-        yy = center[1]
-        color.append("red")
-        area.append(300)
         x.append(xx)
         y.append(yy)
 
     plt.scatter(x,y,c=color,s=area)
-    
-    x = np.array(range(0, 3000))  
+    plt.xlabel(xAxis)
+    plt.ylabel(yAxis)
+    plt.title(title)
+
+    color=[]
+    x=[]
+    y=[]
+    area=[]
+
+    for idx, center in enumerate(clusterCenters):
+        xx = center[0]
+        yy = center[1]
+        color.append(COLORS[idx])
+        area.append(300)
+        x.append(xx)
+        y.append(yy)
+
+    plt.scatter(x,y,c=color,s=area, marker='s', linewidths=3.0)
+
+
+'''
+plot clustering of artist songs based on diversity and size
+'''
+def plotDiversitySizeClustering(result, clusterCenters, xAxis="x", yAxis="y", title=''):
+
+    plot2DSparkCluster(result, clusterCenters, xAxis, yAxis, title)
+
+    # define region
+    x = np.array(range(0, 3000))
     y = eval('x')
-    plt.plot(x, y, c='white')  
+    plt.plot(x, y, c='white')
     plt.ylim((-50,720))
     plt.xlim((-50,2500))
-    
+
     y1 = x
     y2 = 1
     plt.fill_between(x, y1, y2, color = 'blue', alpha= '0.1')
-    
+
     plt.show()
-    
-    
-    
-    
-    
-    
-    
+
+
+def autolabel(rects):
+
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
+                '%d' % int(height),
+                ha='center', va='bottom')
+
+
+'''
+Bar chart
+ref: http://matplotlib.org/examples/api/barchart_demo.html
+'''
+def barChartArtistCountInCluster(artistSongsClustering, clusters):
+    clusterNb = len(clusters)
+    label = []
+    counts = {}
+    bars = []
+    fig, ax = plt.subplots()
+
+    N = len(artistSongsClustering)
+    ind = np.arange(N)
+    width = 0.20
+
+    # init dictionary of cluster counts
+    for c in range(len(clusters)):
+        counts[c] = []
+
+    # add for each artist the count for each cluster
+    for artist, clusterCounts in artistSongsClustering.items():
+        label.append(artist)
+        print(artist, clusterCounts)
+        for c, cCount in clusterCounts.items() :
+            counts[int(c)].append(cCount)
+    n = 0;
+    for cluster, cCount in counts.items():
+        bars.append(ax.bar(ind + width*n, cCount, width, color=COLORS[int(cluster)]))
+        n+=1
+
+    # add some text for labels, title and axes ticks
+    ax.set_ylabel('Nb of Songs')
+    ax.set_title('Artist Song Count for Each Cluster ')
+    ax.set_xticks(ind + width /clusterNb)
+    ax.set_xticklabels(artistSongsClustering.keys())
+
+    allBars = map(lambda x: x[0], bars)
+    ax.legend(allBars, [str(k) + " - " + str(v) for k, v in enumerate(clusters)])
+
+#     for bar in bars:
+#         autolabel(bar)
+
+    plt.show()
+
+
+
+
+
+
+
+
